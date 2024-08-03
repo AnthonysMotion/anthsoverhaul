d386b078f04a667f1531e1b4aefee92d34ba77dc
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-08-03 14:33:02 +1200

    base ver

--- src/main/java/net/anth/anthsoverhaul/Anthsoverhaul.java
+++ src/main/java/net/anth/anthsoverhaul/Anthsoverhaul.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 public class Anthsoverhaul implements ModInitializer {
 	public static final String MOD_ID = "anthsoverhaul";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	// base ver
 
 	@Override
 	public void onInitialize() {
