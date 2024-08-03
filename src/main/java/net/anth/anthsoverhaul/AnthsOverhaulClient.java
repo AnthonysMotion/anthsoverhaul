a200eedf8af21b8b1e470fc489c9ef487e955773
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-08-03 15:15:02 +1200

    add client

---
+++ src/main/java/net/anth/anthsoverhaul/AnthsOverhaulClient.java
@@ -0,0 +1,11 @@
+package net.anth.anthsoverhaul;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AnthsOverhaulClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
